a7bfcd8af4a9701b6688028e7091174a181d42f8
imanuel <imanuelkalvin@gmail.com>
2025-09-21 20:25:42 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -1,24 +1,29 @@
 package com.example.lab_week_04
 
+import android.content.Context
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 val TABS_FIXED = listOf(
     R.string.starbucks_title,
     R.string.janjijiwa_title,
-    R.string.kopikenangan_title
+    R.string.kopikenangan_title,
 )
 
-class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
-    FragmentStateAdapter(fragmentManager, lifecycle) {
+val TAB_CONTENTS = listOf(
+    R.string.starbucks_desc,
+    R.string.janjijiwa_desc,
+    R.string.kopikenangan_desc,
+)
 
-    override fun getItemCount(): Int {
-        return TABS_FIXED.size
-    }
+class CafeAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    private val context: Context = fragment.requireContext()
+
+    override fun getItemCount(): Int = TABS_FIXED.size
 
     override fun createFragment(position: Int): Fragment {
-        return CafeDetailFragment()
+        val content = context.getString(TAB_CONTENTS[position])
+        return CafeDetailFragment.newInstance(content)
     }
 }
